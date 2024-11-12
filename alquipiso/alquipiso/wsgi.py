0a8dc13abfa4d36bce9cd53f55afd033184b1b35
alenicbra <alenicbra@alum.us.es>
2024-11-12 21:59:55 +0100

    first commit

---
+++ alquipiso/alquipiso/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for alquipiso project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'alquipiso.settings')
+
+application = get_wsgi_application()
